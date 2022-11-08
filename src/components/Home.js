c3667fe6f84d9ad13d2a47ab28ff37ba021eaa48
Majson <majsonlbn@gmail.com>
2022-11-08 19:25:20 +0100

    how it works section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
-import  ReactDOM  from 'react-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core';
+//import  ReactDOM  from 'react-dom';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core';
 import Header from './Header.js';
 import '../styles/Home.scss';
 import React from 'react';
+import fish from '../images/fish.png';
+import spaghetti from '../images/spaghetti.png';
+import pizza from '../images/pizza.png';
+import dish from '../images/dish.png';
+import fries from '../images/fries.png';
 function Home() {
     return (
         <div className = "Home">
@@ -27,7 +32,117 @@ function Home() {
                 <div id = "rightMain">
                 
                 </div>
-            </div>
+                </div>
+                <div id = "howItWorks">
+                    <span id = "howItWorksSpan">How it works</span>
+                    <p>Lorem Ipsum is simply dummy text of the prinitng and typersetting industry<br></br>
+                    Lorem Ipsum has been the industry</p>
+                    <div id = "orderNow" className = "howItWorks">
+                        <div id = "orderNowCircle" className = "howItWorksCircles"></div>
+                            <span>Order Now</span>
+                            <p>Lorem Ipsum is a simply dummy text of the<br></br>
+                            printing and typersetting industry. Lorem Ipsum has been the industry</p>
+                        
+                    </div>
+                    <div id = "getDelivery" className = "howItWorks">
+                    <div id = "getDeliveryCircle" className = "howItWorksCircles"></div>
+                            <span>Get Delivery</span>
+                            <p>Lorem Ipsum is a simply dummy text of the<br></br>
+                            printing and typersetting industry. Lorem Ipsum has been the industry</p>
+                        
+                        </div>
+                        <div id = "payment" className = "howItWorks">
+                        <div id = "paymentCircle" className = "howItWorksCircles"></div>
+                            <span>Payment</span>
+                            <p>Lorem Ipsum is a simply dummy text of the<br></br>
+                            printing and typersetting industry. Lorem Ipsum has been the industry</p>
+                        </div>
+                </div>
+                <div id = "menu">
+                    <span>Our Delicious Dish</span>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting<br></br> industry. Lorem Ipsum has been the industry</p>
+                    <div className = "menuItems">
+                        <img src = {fish}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {fries}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {pizza}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {dish}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {spaghetti}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {fish}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {pizza}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                    <div className = "menuItems">
+                        <img src = {fries}></img>
+                        
+                        <span>New York Vega</span>
+                        <p>Lorem Ipsum is simply dummy text of<br></br>
+                        the printing and typersetting...</p>
+                        <span>$7.10</span>
+                        <button className = "addToCart"></button>
+                        <button className = "shareDish"></button>
+                    </div>
+                </div>
+            
         </div>
     )
 }
