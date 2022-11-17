d01a85a4b26363439016fffbad600405770328c6
Majson <majsonlbn@gmail.com>
2022-11-17 21:10:39 +0100

    blog articles styling

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -19,10 +19,133 @@ function Blog() {
                 <span>5</span>
                 <img src = {person}></img>
                 <span>Michal Majewski</span>
+
                 <p>This book is a treatise on the theory of ethics</p>
+
+                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta<br></br>
+                test. Override the digital divide with additional clickthroughs from DevOps<br></br>
+                Nanotechnology immersion along the information highway will close the loop on<br></br>
+                focusing solely on the bottom line.</p>
+
+                <span>Continue Reading</span>
+            </div>
+            <div className = "articles">
+                <img src = {articleImg}></img>
+                <img src = {calendar}></img>
+                <span>21 Jun 2022</span>
+                <img src = {comment}></img>
+                <span>5</span>
+                <img src = {person}></img>
+                <span>Michal Majewski</span>
+
+                <p>This book is a treatise on the theory of ethics</p>
+
+                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta<br></br>
+                test. Override the digital divide with additional clickthroughs from DevOps<br></br>
+                Nanotechnology immersion along the information highway will close the loop on<br></br>
+                focusing solely on the bottom line.</p>
+
+                <span>Continue Reading</span>
+            </div>
+            <div className = "articles">
+                <img src = {articleImg}></img>
+                <img src = {calendar}></img>
+                <span>21 Jun 2022</span>
+                <img src = {comment}></img>
+                <span>5</span>
+                <img src = {person}></img>
+                <span>Michal Majewski</span>
+
+                <p>This book is a treatise on the theory of ethics</p>
+
+                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta<br></br>
+                test. Override the digital divide with additional clickthroughs from DevOps<br></br>
+                Nanotechnology immersion along the information highway will close the loop on<br></br>
+                focusing solely on the bottom line.</p>
+
+                <span>Continue Reading</span>
+            </div>
+            <div className = "articles">
+                <img src = {articleImg}></img>
+                <img src = {calendar}></img>
+                <span>21 Jun 2022</span>
+                <img src = {comment}></img>
+                <span>5</span>
+                <img src = {person}></img>
+                <span>Michal Majewski</span>
+
+                <p>This book is a treatise on the theory of ethics</p>
+
+                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta<br></br>
+                test. Override the digital divide with additional clickthroughs from DevOps<br></br>
+                Nanotechnology immersion along the information highway will close the loop on<br></br>
+                focusing solely on the bottom line.</p>
+
+                <span>Continue Reading</span>
+            </div>
+            <div className = "articles">
+                <img src = {articleImg}></img>
+                <img src = {calendar}></img>
+                <span>21 Jun 2022</span>
+                <img src = {comment}></img>
+                <span>5</span>
+                <img src = {person}></img>
+                <span>Michal Majewski</span>
+
+                <p>This book is a treatise on the theory of ethics</p>
+
+                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta<br></br>
+                test. Override the digital divide with additional clickthroughs from DevOps<br></br>
+                Nanotechnology immersion along the information highway will close the loop on<br></br>
+                focusing solely on the bottom line.</p>
+
+                <span>Continue Reading</span>
+            </div>
+            <div id = "categories">
+                <input type = "text" id = "search" placeholder = "Search here"></input>
+                <span>Categories</span>
+                <button id = "burger" className = "categories">
+                    <span>Burger</span>
+                    <span>(7)</span>
+                </button>
+                <button id = "chicken" className = "categories">
+                    <span>Chicken</span>
+                    <span>(9)</span>
+                </button>
+                <button id = "drinks" className = "categories">
+                    <span>Drinks</span>
+                    <span>(3)</span>
+                </button>
+                <button id = "fries" className = "categories">
+                    <span>Fries</span>
+                    <span>(8)</span>
+                </button>
+                <button id = "pizza" className = "categories">
+                    <span>Pizza</span>
+                    <span>(5)</span>
+                </button>
+                <button id = "sandwich" clasName = "categories">
+                    <span>Sandwich</span>
+                    <span>(3)</span>
+                </button>
+
+                <span>Archive</span>
+                <button id = "august" className = "categories">
+                    <span>August 2022</span>
+                    <span>(6)</span>
+                </button>
+                <button id = "july" className = "categories">
+                    <span>July 2022</span>
+                    <span>(1)</span>
+                </button>
+                <button id = "june" className = "categories">
+                    <span>June 2022</span>
+                    <span>(2)</span>
+                </button>
             </div>
             
         </div>
+        
     )
 }
 
