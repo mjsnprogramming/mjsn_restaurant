63e2de4c870cc72303319eb3b96019da0e096918
Majson <majsonlbn@gmail.com>
2022-11-19 22:27:04 +0100

    menu page started

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,20 @@ function Menu() {
     return (
         <div className = "Menu">
             <HeaderMenu/>    
-            
+            <div id = "menuHeaderImg">
+                <span>Menu</span>
+            </div>
+            <div id = "menuNavbar">
+                <ul>
+                    <li><button tabIndex={0}>All</button></li>
+                    <li><button tabIndex={0}>Breakfast</button></li>
+                    <li><button tabIndex={0}>Lunch</button></li>
+                    <li><button tabIndex={0}>Dinner</button></li>
+                    <li><button tabIndex={0}>Drinks</button></li>
+                    <li><button tabIndex={0}>Our Special Recipes</button></li>
+                    <li><button tabIndex={0}>Others</button></li>
+                </ul>
+            </div>
         </div>
     )
 }
